bc5ad9aed2c2e77501d83a26de6f51d9c5945c4e
Mithilesh Sawant <mithileshsawant2907@gmail.com>
2025-10-22 13:02:27 +0530

    wq
    wq

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import Login from './pages/login.jsx'
-import { MonthlyReport, ProfileCard, QuickAccess, RecentExpenses, Sidebar } from './pages/Home.jsx'
+import { MonthlyReport, ProfileCard, QuickAccess, RecentExpenses, Sidebar } from './pages/HomeComponents.jsx'
+import HomePage from './pages/Home.jsx'
 
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MonthlyReport />  
+    <HomePage />  
   </StrictMode>,
 )
