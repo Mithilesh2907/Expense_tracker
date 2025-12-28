9833d8ec78ddb5507343003ab41ff8eeed2a685f
Mithilesh Sawant <mithileshsawant2907@gmail.com>
2025-12-28 15:57:32 +0530

    minor changes

--- frontend/src/context.jsx
+++ frontend/src/context.jsx
@@ -5,7 +5,7 @@ import { supabase } from "./supabase";
 const AuthContext = createContext(null);
 export const useAuth = () => useContext(AuthContext);
 
-export const AuthProvider = ({ Children }) => {
+export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [session, setSession] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -17,15 +17,16 @@ export const AuthProvider = ({ Children }) => {
             setLoading(false);
         });
 
-        const {data: listener} = supabase.auth.onAuthStateChange(
-            (_event, session) => {
-                setSession(session);
-                setUser(session?.user ?? null);
-                setLoading(false);
+        const { data: { subscription } } = supabase.auth.onAuthStateChange(
+        (_event, session) => {
+        setSession(session);
+        setUser(session?.user ?? null);
+        setLoading(false);
             }
         );
+
         return () => {
-            listener?.unsubscribe();
+        subscription?.unsubscribe();
         };
     }, []);
 
